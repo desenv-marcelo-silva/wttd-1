764bc50fb8bd4139a97127a7bcfa7485d9b75fac
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-11-29 21:59:17 -0200

    Inclusão de testes na primeira página.

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class homeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """Must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use index.html as template"""
+        self.assertTemplateUsed(self.response, 'index.html')
